daa4ea323a1767816cba254c8403a9b028766a50
Prakash Dwivedi <prakash.dwivedi@districtd.in>
2026-09-11 01:23:00 +0530

    setup of typescript and backend server

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express, {type Express, type Request, type Response} from "express";
+
+const app: Express = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json())  //Middleware to parse json payLoads.
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Welcome to EventHub backend API");
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on PORT: ${PORT}`);
+})
